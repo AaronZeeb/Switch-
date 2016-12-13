ce6148d40a207bf84e26ab28c3671a39dbaa64b2
AaronZeeb <aaronzeeb2@gmail.com>
2016-12-13 10:25:03 -0600

    Commit

---
+++ Switch/Switch_Client.java
@@ -0,0 +1,13 @@
+public class Switch_Client
+{
+    public int getSwitchState (int switch){
+    
+    }
+    public int getLampState(){
+    
+    } 
+    
+    public void toggleSwitch(int switch){
+    
+    }
+}
